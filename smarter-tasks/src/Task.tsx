24e101ad0269475bc9e38f706a42430328b04a15
krinal-mistri <sutharkirinal@gmail.com>
2024-11-09 09:54:45 +0530

    Level-3

---
+++ smarter-tasks/src/Task.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Task extends React.Component {
+  render() {
+    return <div className="mx-auto w-67 text-lg">Buy groceries </div>;
+  }
+}
+
+export default Task;
